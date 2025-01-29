92a21fa59c90f5cd17d29461b9d873592737df2a
Sean Kooyman <github@teachthe.net>
2025-01-29 08:14:38 -0500

    update next/previous buttons

--- bible-view.php
+++ bible-view.php
@@ -201,29 +201,6 @@ if ($currentVerse && $currentVerse != 'all') {
                     </select>
                 </div>
             </div>
-            <a target='_blank' href="https://github.com/HistoricalChristianFaith" class="github-corner" title="This website and its database are open source! Click to open it on Github!">
-                <svg width="80" height="80" viewBox="0 0 250 250" style="fill:#151513; color:#fff; position: absolute; top: 0; border: 0; right: 0;" aria-hidden="true">
-                    <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"/>
-                    <path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"/>
-                    <path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"/>
-                </svg>
-            </a>
-            <style>
-                .github-corner:hover .octo-arm {
-                    animation: octocat-wave 560ms ease-in-out;
-                }
-                @keyframes octocat-wave {
-                    0%, 100% { transform: rotate(0); }
-                    20%, 60% { transform: rotate(-25deg); }
-                    40%, 80% { transform: rotate(10deg); }
-                }
-
-                @media (max-width: 768px) {
-                    .github-corner {
-                        display: none;
-                    }
-                }
-            </style>
 
         </header>
 
@@ -232,17 +209,60 @@ if ($currentVerse && $currentVerse != 'all') {
                 <?= getBibleText($formattedCurrentBook, $currentChapter, $currentVerse) ?>
             </div>
             <div class="row">
-                <div class="col" style="<?= $currentVerse && $currentVerse != 'all' ? 'visibility: hidden;' : '' ?>">
-                    <a href="/<?= urlencode($formattedCurrentBook) ?>/<?= $prevChapter ?>/all" class="btn nav-button w-100 <?= is_null($prevChapter) ? 'disabled' : '' ?>">Previous Chapter</a>
+                <?php
+
+                    if (!$currentVerse || $currentVerse == 'all') {
+                        //if current verse = all, do next/previous chapter
+                        $prev_url = "/".urlencode($formattedCurrentBook)."/".$prevChapter."/all";
+                        $next_url = "/".urlencode($formattedCurrentBook)."/".$nextChapter."/all";
+
+                        if (!$prevChapter) {
+                            // We're already at the first chapter in a book...
+                            // So... just do nothing.
+                            $prev_url = "/".urlencode($formattedCurrentBook)."/".$currentChapter."/all";
+                        }
+
+                        if (!$nextChapter) {
+                            // We're already at the last chapter in a book...
+                            // So... just do nothing.
+                            $next_url = "/".urlencode($formattedCurrentBook)."/".$currentChapter."/all";
+                        }
+                    }
+                    else {
+                        // Default to next/previous verse
+                        $prev_url = "/".urlencode($formattedCurrentBook)."/".$currentChapter."/".$prevVerse;
+                        $next_url = "/".urlencode($formattedCurrentBook)."/".$currentChapter."/".$nextVerse;
+
+                        if (!$prevVerse) {
+                            // We're already at the first verse in a chapter. 
+                            // So... let's just load the prior chapter
+                            $prev_url = "/".urlencode($formattedCurrentBook)."/".$prevChapter."/all";
+
+                            if (!$prevChapter) {
+                                // We're already at the first chapter in a book...
+                                // So... just do nothing.
+                                $prev_url = "/".urlencode($formattedCurrentBook)."/".$currentChapter."/".$currentVerse;
+                            }
+                        }
+
+                        if (!$nextVerse) {
+                            // We're already at the last verse in a chapter. 
+                            // So... let's just load the next chapter
+                            $next_url = "/".urlencode($formattedCurrentBook)."/".$nextChapter."/all";
+
+                            if (!$nextChapter) {
+                                // We're already at the last chapter in a book...
+                                // So... just do nothing.
+                                $next_url = "/".urlencode($formattedCurrentBook)."/".$currentChapter."/".$currentVerse;
+                            }
+                        }
+                    }
+                ?>
+                <div class="col">
+                    <a href="<?= $prev_url ?>" class="btn nav-button w-100">Previous</a>
                 </div>
-                <div class="col" style="<?= !$currentVerse || $currentVerse == 'all' ? 'visibility: hidden;' : '' ?>">
-                    <a href="/<?= urlencode($formattedCurrentBook) ?>/<?= $currentChapter ?>/<?= $prevVerse ?>" class="btn nav-button w-100 <?= is_null($prevVerse) ? 'disabled' : '' ?>">Previous Verse</a>
-                </div>
-                <div class="col" style="<?= !$currentVerse || $currentVerse == 'all' ? 'visibility: hidden;' : '' ?>">
-                    <a href="/<?= urlencode($formattedCurrentBook) ?>/<?= $currentChapter ?>/<?= $nextVerse ?>" class="btn nav-button w-100 <?= is_null($nextVerse) ? 'disabled' : '' ?>">Next Verse</a>
-                </div>
-                <div class="col" style="<?= $currentVerse && $currentVerse != 'all' ? 'visibility: hidden;' : '' ?>">
-                    <a href="/<?= urlencode($formattedCurrentBook) ?>/<?= $nextChapter ?>/all" class="btn nav-button w-100 <?= is_null($nextChapter) ? 'disabled' : '' ?>">Next Chapter</a>
+                <div class="col">
+                    <a href="<?= $next_url ?>" class="btn nav-button w-100">Next</a>
                 </div>
             </div>
         </main>
@@ -264,4 +284,12 @@ if ($currentVerse && $currentVerse != 'all') {
     });
     </script>
 </body>
+<footer class="footer py-3 mt-4">
+    <div class="container text-center">
+        <p>
+            Powered by the open source, crowd-sourced 
+            <a href="https://github.com/HistoricalChristianFaith/Commentaries-Database" target='_blank'>HistoricalChristianFaith/Commentaries-Database</a>
+        </p>
+    </div>
+</footer>
 </html>
